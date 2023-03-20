2155ffbe376b4d0b80b9b73f091a5f92424a9d85
Rowan <rowanmoss04@gmail.com>
2023-03-20 14:17:50 +0000

    Adds movement to player using arrow keys and rotation
    - takes arrow key input
    - rotates the player
    - then moves them in the direction of arrow key

--- maze-game/MyWorld.java
+++ maze-game/MyWorld.java
@@ -11,11 +11,21 @@ public class MyWorld extends World
 
     /**
      * Constructor for objects of class MyWorld.
-     * 
      */
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(1200, 800, 1);
+        addPlayer();
+    }
+    
+    /**
+     * adds the player
+     */
+    private void addPlayer()
+    {
+        int x = 15;
+        int y = 30;
+        addObject(new Player(), x, y);
     }
 }
